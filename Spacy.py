8a8973e54b2ae5c6ef645d059fe73c1a0b46a639
sharath298off <sharathwork298@gmail.com>
2025-06-16 17:26:27 -0500

    Update Spacy.py

--- Spacy.py
+++ Spacy.py
@@ -3,7 +3,7 @@ import spacy
 # Load the small English model
 nlp = spacy.load("en_core_web_sm")
 
-# Input sentence
+# Input sentnce
 sentence = "Barack Obama served as the 44th President of the United States and won the Nobel Peace Prize in 2009."
 
 # Process the sentence
